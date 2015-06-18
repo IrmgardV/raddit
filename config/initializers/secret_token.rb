a9e61cdf86780bd2452fb14361ae3740989e25d8
irmgard vandepitte <irmvdp@hotmail.com>
2015-06-18 08:16:02 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raddit::Application.config.secret_key_base = '24a586aac5bbf6dc723975e2cb6bfb33b36f761ae3683c032c2c6b7d997d7efeea1ab7a198c83e6c3df750a356cbe510d50fb836a8ad6c18c1de4d6e9ff170de'
